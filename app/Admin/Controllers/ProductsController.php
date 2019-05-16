265c14d4de0e817233b890620b7fccc90fe119b7
jasongzj <jasongzj@163.com>
2019-05-16 07:29:10 +0000

    后台商品管理代码封装

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -4,69 +4,22 @@ namespace App\Admin\Controllers;
 
 use App\Models\Category;
 use App\Models\Product;
-use App\Http\Controllers\Controller;
-use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
-use Encore\Admin\Show;
-use function GuzzleHttp\Psr7\uri_for;
 
-class ProductsController extends Controller
+class ProductsController extends CommonProductsController
 {
-    use HasResourceActions;
 
-    /**
-     * Index interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function index(Content $content)
+    public function getProductType()
     {
-        return $content
-            ->header('商品列表')
-            ->body($this->grid());
+        return Product::TYPE_NORMAL;
     }
 
-    /**
-     * Edit interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
+
+    protected function customGrid(Grid $grid)
     {
-        return $content
-            ->header('编辑商品')
-            ->body($this->form()->edit($id));
-    }
-
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('创建商品')
-            ->body($this->form());
-    }
-
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        $grid = new Grid(new Product);
-
-        $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
-
+        $grid->model()->with(['category']);
         $grid->id('Id')->sortable();
         $grid->title('商品名称');
         $grid->column('category.name', '所属类目');
@@ -77,51 +30,11 @@ class ProductsController extends Controller
         $grid->rating('评分');
         $grid->sold_count('销量');
         $grid->review_count('评论数');
-
-        $grid->actions(function ($actions) {
-            $actions->disableView();
-            $actions->disableDelete();
-        });
-
-
-        return $grid;
     }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
+
+    protected function customForm(Form $form)
     {
-        $form = new Form(new Product);
 
-        $form->hidden('type')->value(Product::TYPE_NORMAL);
-
-        $form->text('title', '商品名称')->rules('required');
-        $form->select('category_id', '所属类目')->options(function ($id) {
-            $category = Category::query()->find($id);
-            if ($category) {
-                return [$category->id => $category->full_name];
-            }
-        })->ajax('/admin/api/categories?is_directory=0');
-        $form->image('image', '封面图片')->rules('required|image');
-        $form->editor('description', '商品描述')->rules('required');
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
-        $form->hasMany('skus', 'SKU列表', function (Form\NestedForm $form) {
-            $form->text('title', 'sku 名称')->rules('required');
-            $form->text('description', 'sku 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|numeric|min:0.01');
-            $form->text('stock', '库存')->rules('required|integer|min:0');
-        });
-
-        // 定义事件回调，在模型即将保存时触发
-        $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)
-                ->min('price') ?: 0;
-        });
-
-
-        return $form;
     }
 }
