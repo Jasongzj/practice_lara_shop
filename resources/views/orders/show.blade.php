af005c2c33a4df7671b2da4a8d0b1035c2c028bf
jasongzj <jasongzj@163.com>
2019-06-05 13:54:30 +0000

    前台付款详情页

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -241,8 +241,7 @@
                 //调用分期接口
                 axios.post('{{ route('payment.installment', ['order' => $order->id]) }}', { count: $(this).data('count')})
                     .then(function (response) {
-                        console.log(response.data)
-                        // 跳转分期付款页面
+                        location.href = '/installments/' + response.data.id;
                     })
             });
         });
